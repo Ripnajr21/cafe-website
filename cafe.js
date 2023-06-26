3c7a13cc7c844c7f7215018991cc967293228bd4
Ripnajr21 <118796362+Ripnajr21@users.noreply.github.com>
2023-06-26 18:16:03 +0530

    Add files via upload

---
+++ cafe.js
@@ -0,0 +1,14 @@
+
+
+function loginPage(form) {
+if (form.userName.value=="username") { 
+if (form.passWord.value=="password") {              
+location="home.html" 
+} else {
+alert("Incorrect detail Password")
+}
+} else {  alert("Incorrect detail Username")
+}
+}
+
+
